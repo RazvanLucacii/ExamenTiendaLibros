2d1190c7792dde1c911457fa971dcd1abe64ace3
RazvanLucacii <rlucaci48@gmail.com>
2024-03-21 14:22:35 +0100

    examen

--- ExamenTiendaLibros/Models/VistaPedidosModel.cs
+++ ExamenTiendaLibros/Models/VistaPedidosModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ExamenTiendaLibros.Models
